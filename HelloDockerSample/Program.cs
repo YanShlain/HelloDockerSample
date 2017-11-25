b4e9c584b558b250a37ec852bc4b67e99f0cc58b
Yan Shlain <yans@stuccomedia.com>
2017-11-25 16:38:58 +0200

    Add project files.

---
+++ HelloDockerSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDockerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
